213f780c91a784e52df8e7a7ae6f3f4b3bbc7c4b
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-06 21:07:14 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 require("units/hellfire");
 
-var lastStand = extend (SectorPreset, "exotic-mod-last-stand", Planets.serpulo, 69, {
+var lastStand = extend(SectorPreset, "exotic-mod-last-stand", Planets.serpulo, 69, {
   localizedName: "Last Stand",
   difficulty: 25, 
   captureWave: 150,
